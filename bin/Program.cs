6168be2a1d70e3d4a36ed4fa1dcd863827a66148
bearddan2000 <67988550+bearddan2000@users.noreply.github.com>
2020-12-01 23:03:38 -0500

    Add files via upload

---
+++ bin/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data;
+using Mono.Data.Sqlite;
+
+namespace CSharpConsol
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            const string connectionString = "URI=file:person.db";
+            IDbConnection dbcon = new SqliteConnection(connectionString);
+            dbcon.Open();
+            using (IDbCommand dbcmd = dbcon.CreateCommand())
+            {
+                const string sql = "SELECT name, age FROM person";
+                dbcmd.CommandText = sql;
+                using (IDataReader reader = dbcmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        string name = reader.GetString(0);
+                        int age = reader.GetInt32(1);
+                        Console.WriteLine("Name: {0}, Age: {1}", name, age);
+                    }
+                }
+            }
+            dbcon.Close();
+        }
+    }
+}
